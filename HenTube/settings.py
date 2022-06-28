87af0b8dfdfe779eceb227094c1cb7dabab81e9f
Hentai <gr801821@gmail.com>
2022-06-29 00:03:32 +0300

    Create Profile app, Video app, Channel app, base template, index view in Video app

--- HenTube/settings.py
+++ HenTube/settings.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Videos.apps.VideosConfig',
+    'Profiles.apps.ProfilesConfig',
+    'Channel.apps.ChannelConfig'
 ]
 
 MIDDLEWARE = [
